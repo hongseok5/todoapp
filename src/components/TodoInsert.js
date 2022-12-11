c6b1cc683d0ba3c67fc86f2fb63a6ea7cce6261d
hongseok5 <ohho28@gmail.com>
2022-12-11 22:40:17 +0900

    test

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,32 @@
+import React, { useState, useCallback } from 'react';
+import './TodoInsert.scss';
+import {MdAdd} from 'react-icons/md';
+
+const TodoInsert = ( {onInsert} ) => {
+
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback( e => {
+        setValue(e.target.value);
+    }, [])
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+            e.preventDefault();
+        },
+        [onInsert, value]
+    )
+    
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input value={value} onChange={onChange} placeholder='Insert To do' />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    )
+}
+
+export default TodoInsert;
---
+++ src/components/TodoListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { MdCheckBox, MdRemoveCircleOutline, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import './TodoListItem.scss';
+import cn from 'classnames';
+
+const TodoListItem = ( {todo, onRemove, onToggle }) => {
+
+    const { id, text, checked } = todo;
+    return (
+        <div className='TodoListItem'>
+            <div className={cn('checkbox',{checked})} onClick={() => onToggle(id)}>
+                { checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank/>}
+                <div className='text'> {text}</div>
+
+            </div>
+            <div className='remove' onClick= {() => onRemove(id)}>
+                <MdRemoveCircleOutline />
+            </div>
+        </div>
+    )
+}
+
+export default React.memo(TodoListItem); // 컴포넌트 최적화, todo, onRemove, onToggle 가 바뀌지 않으면 리렌더링 안 함.
